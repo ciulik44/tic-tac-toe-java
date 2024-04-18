a312506aa8ea317c0fdf1bc74fbf397e44ec2eda
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-18 19:59:15 +0200

    new file:   main.class 	new file:   main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+class main {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int row =0; row < board.length; row++) {
+            for(int col = 0; col <board[row].length; col++) {
+                board[row][col] = ' ';
+            }
+        }
+    }
+}
